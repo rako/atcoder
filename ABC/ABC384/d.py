d3291871ea9877376ae854c6df682e6723d95c7e
rako <=>
2024-12-15 23:34:52 +0900

    修正

--- ABC/ABC384/d.py
+++ ABC/ABC384/d.py
@@ -4,15 +4,15 @@ a = list(map(int, input().split()))
 a2 = a*2
 accma2 = list(itertools.accumulate(a2))
 
-sum_a = sum(a)
-nk = s % sum_a
-#尺取り
-r, ans, tmp = 0, 0, 1
-for l in range(n*2):
-    while r < n*2 and accma2[r+1] - accma2[l] < nk:
-        r += 1
-    print(l,r)
-    if accma2[r+1] - accma2[l] == nk:
+s %= sum(a) #sがsum(a)を超える場合の処理です。これを忘れるとWAになります。
+
+#尺取り法
+#print(sum_a)
+ans = 0
+for i in accma2:
+    if i + s in accma2: #累積和の中にi+sがあるかどうか。このsをsum_aにしていたのが間違いだと思う。
         ans = 1
         break
+    else:
+        continue
 print("Yes" if ans == 1 else "No")

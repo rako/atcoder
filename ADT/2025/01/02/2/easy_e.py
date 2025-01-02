6737a8d5ffd474ad03824d54b7fb4a13af85c288
rako <rakoinfotech@gmail.com>
2025-01-02 23:10:11 +0900

    7WAだけ

--- ADT/2025/01/02/2/easy_e.py
+++ ADT/2025/01/02/2/easy_e.py
@@ -3,11 +3,9 @@ xylist = [list(map(int, input().split())) for _ in range(n)]
 s = input()
 for i in range(n):
     xylist[i].append(s[i])
-#print(xylist)
 #yが同じで、xが違うものを探す
 #より小さい方のxがRで、より大きい方のxがLならば、衝突する
 xylist.sort(key=lambda x: x[1])
-print(xylist)
 i = 0
 existlist = []
 while(i < n):
@@ -20,17 +18,18 @@ while(i < n):
         j += 1
     tmplist = []
     for k in range(len(eqlist)):
-        print(eqlist[k])
-        print(tmplist)
-        if len(tmplist) == 0 or eqlist[k][2] == "R":
+        #print(eqlist[k])
+        #print(tmplist)
+        if len(tmplist) == 0:
             tmplist.append(eqlist[k][2])
-            continue
+        elif eqlist[k][2] == "R":
+            tmplist.append(eqlist[k][2])
         elif eqlist[k][2] == "L":
             if tmplist[-1] == "R":
                 tmplist.pop(-1)
             else:
-                
-    existlist.append(True if len(tmplist) != 0 else False)
+                tmplist.append(eqlist[k][2])
+    existlist.append(True if len(tmplist) != len(eqlist) else False)
     i = j
-print(existlist)
+#print(existlist)
 print("Yes" if any(existlist) else "No")
